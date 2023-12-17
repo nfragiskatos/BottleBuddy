9f05b5f1dec706cb41829e9e68a0f89fb47ddf7b
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-12-17 08:15:55 -0700

    - Added notes to the FeedingItem view. - Made notes section expandable via button click if the note overflows a single line.

--- app/src/main/java/com/nicholasfragiskatos/feedme/ui/screens/home/FeedingItem.kt
+++ app/src/main/java/com/nicholasfragiskatos/feedme/ui/screens/home/FeedingItem.kt
@@ -1,5 +1,8 @@
 package com.nicholasfragiskatos.feedme.ui.screens.home
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,14 +12,24 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.nicholasfragiskatos.feedme.domain.model.Feeding
 import com.nicholasfragiskatos.feedme.domain.model.UnitOfMeasurement
@@ -31,6 +44,14 @@ fun FeedingItem(
     onClick: () -> Unit,
 ) {
 
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    var isExpandable by remember {
+        mutableStateOf(false)
+    }
+
     val formatter = remember {
         SimpleDateFormat("h:mm a")
     }
@@ -43,48 +64,79 @@ fun FeedingItem(
             },
     ) {
         Surface(tonalElevation = 8.dp) {
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
+            Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-
-                ) {
-                Column(
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .animateContentSize(
+                        animationSpec = spring(
+                            dampingRatio = Spring.DampingRatioMediumBouncy,
+                            stiffness = Spring.StiffnessLow
+                        )
+                    )
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
                     modifier = Modifier
-                        .fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceBetween,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Text(
-                        text = "%.2f".format(
-                            UnitUtils.convertMeasurement(
-                                feeding.quantity,
-                                feeding.unit,
-                                displayUnit
-                            )
-                        ),
-                        style = MaterialTheme.typography.titleLarge,
-                    )
-                    Text(
-                        text = displayUnit.abbreviation,
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                }
+                        .fillMaxSize()
 
-                val date = formatter.format(feeding.date)
-
-                Column(
-                    modifier = Modifier.fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceBetween,
-                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Text(text = date, style = MaterialTheme.typography.titleMedium)
-                    TextButton(onClick = { onDelete(feeding) }) {
-                        Text(text = "Delete", style = MaterialTheme.typography.labelLarge)
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight(),
+                        verticalArrangement = Arrangement.SpaceBetween,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(
+                            text = "%.2f".format(
+                                UnitUtils.convertMeasurement(
+                                    feeding.quantity,
+                                    feeding.unit,
+                                    displayUnit
+                                )
+                            ),
+                            style = MaterialTheme.typography.titleLarge,
+                        )
+                        Text(
+                            text = displayUnit.abbreviation,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                        if (isExpandable || expanded) {
+                            IconButton(onClick = { expanded = !expanded }) {
+                                Icon(
+                                    imageVector = if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                                    contentDescription = if (expanded) "hide notes" else "show notes"
+                                )
+                            }
+                        }
+                    }
+
+                    val date = formatter.format(feeding.date)
+
+                    Column(
+                        modifier = Modifier.fillMaxHeight(),
+                        verticalArrangement = Arrangement.SpaceBetween,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(text = date, style = MaterialTheme.typography.titleMedium)
+                        TextButton(onClick = { onDelete(feeding) }) {
+                            Text(text = "Delete", style = MaterialTheme.typography.labelLarge)
+                        }
                     }
                 }
-
+                if (feeding.notes.isNotBlank()) {
+                    Divider()
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = feeding.notes,
+                        style = MaterialTheme.typography.bodyLarge,
+                        maxLines = if (expanded) Int.MAX_VALUE else 1,
+                        overflow = if (expanded) TextOverflow.Visible else TextOverflow.Ellipsis,
+                        onTextLayout = { result ->
+                            isExpandable = result.hasVisualOverflow
+                        }
+                    )
+                }
             }
         }
     }
