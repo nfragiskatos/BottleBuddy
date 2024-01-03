5719c6008e5462b056b511e44c174dc5a69eed11
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2024-01-03 11:25:59 -0700

    - Added impl for new ReportGenerator::generateDaySummary function

--- app/src/test/java/com/nicholasfragiskatos/feedme/utils/FakeReportGenerator.kt
+++ app/src/test/java/com/nicholasfragiskatos/feedme/utils/FakeReportGenerator.kt
@@ -2,13 +2,22 @@ package com.nicholasfragiskatos.feedme.utils
 
 import com.nicholasfragiskatos.feedme.domain.model.Feeding
 import com.nicholasfragiskatos.feedme.domain.model.UnitOfMeasurement
+import java.util.Date
 
-class FakeReportGenerator: ReportGenerator {
+class FakeReportGenerator : ReportGenerator {
     override suspend fun generateFeedingSummary(
         feeding: Feeding,
         displayUnit: UnitOfMeasurement,
-        is24HourFormat: Boolean
+        is24HourFormat: Boolean,
     ): String {
         return "Fake Report"
+    }
+
+    override suspend fun generateDaySummary(
+        date: Date,
+        displayUnit: UnitOfMeasurement,
+        is24HourFormat: Boolean,
+    ): String {
+        return "Fake Day Summary"
     }
 }
